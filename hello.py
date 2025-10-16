fb139760d9bf1a4f11c837d6d778b150526ffd3c
GiovannaKarolline <mennezes.giovanna@gmail.com>
2025-10-16 15:02:49 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -62,7 +62,7 @@ def send_simple_message(to, subject, newUser):
     print('from: ' + str(app.config['API_FROM']), flush=True)
     print('to: ' + str(to), flush=True)
     print('subject: ' + str(app.config['FLASKY_MAIL_SUBJECT_PREFIX']) + ' ' + subject, flush=True)
-    print('text: ' + "Novo usuário cadastrado: " + newUser, flush=True)
+    print('text: ' + "Prontuário: PT303304X\nNome: Giovanna Karolline Menezes Ribeiro\nNovo usuário cadastrado: " + newUser, flush=True)
 
     resposta = requests.post(app.config['API_URL'], 
                              auth=("api", app.config['API_KEY']), data={"from": app.config['API_FROM'], 
@@ -113,7 +113,7 @@ def index():
             print('from: ' + str(app.config['API_FROM']), flush=True)
             print('to: ' + str([app.config['FLASKY_ADMIN'], "flaskaulasweb@zohomail.com"]), flush=True)
             print('subject: ' + str(app.config['FLASKY_MAIL_SUBJECT_PREFIX']), flush=True)
-            print('text: ' + "Prontuário: PT303304X\nNome: Giovanna Karolline Menezes Ribeiro\nNovo usuário cadastrado: " + form.name.data, flush=True)
+            print('text: ' + "Novo usuário cadastrado: " + form.name.data, flush=True)
 
             if app.config['FLASKY_ADMIN'] and mandarEmail:                
                 print('Enviando mensagem...', flush=True)
